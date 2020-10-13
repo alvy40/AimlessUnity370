12741bd69f6dec9a80e106a8b42f794ddcf8b13d
alvy40 <60720394+alvy40@users.noreply.github.com>
2020-10-12 21:18:53 -0400

    AddFilesJa
    Add folders and scripts.

---
+++ Aimless_Unity_Game370_v0.0/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
