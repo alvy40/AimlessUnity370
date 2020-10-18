81803d49d0cc99e0398f2a252226946eeb9f849e
SunnyValentine <66551747+SunnyValentine@users.noreply.github.com>
2020-10-18 13:32:53 -0700

    Pick Up Items
    A script for picking up items

---
+++ Aimless_Unity_Game370_v0.0/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPickup : MonoBehaviour
+{
+    [SerializeField]
+    private Text pickUpText;
+    // Allows player to pick up item when "True"
+    private bool pickUpAllowed;
+
+    //  when walking towards an item
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Press "E" to pick item up
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            PickUp();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        // adds/picks up item
+        Destroy(gameObject);
+    }
+}
