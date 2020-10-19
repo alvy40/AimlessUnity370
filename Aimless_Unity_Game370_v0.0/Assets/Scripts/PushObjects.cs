c21e2563182bf149bb3ee0e76dfd7791a2e2e7b0
SunnyValentine <66551747+SunnyValentine@users.noreply.github.com>
2020-10-18 20:07:44 -0700

    Push/Pull Script
    Made a push object script but I might have to tweak it alittle. You guys can take a look too if you want

---
+++ Aimless_Unity_Game370_v0.0/Assets/Scripts/PushObjects.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushObjects : MonoBehaviour
+{
+	public float distance = 1f;
+	public LayerMask boxMask;
+
+	public GameObject box;
+
+	
+	void Update()
+	{
+
+		Physics2D.queriesStartInColliders = false;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+
+		if (hit.collider != null && hit.collider.gameObject.tag == "pushable" && Input.GetKeyDown(KeyCode.Q))
+		{
+
+
+			box = hit.collider.gameObject;
+			box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+			box.GetComponent<FixedJoint2D>().enabled = true;
+			box.GetComponent<boxpull>().beingPushed = true;
+
+		}
+		else if (Input.GetKeyUp(KeyCode.Q))
+		{
+			box.GetComponent<FixedJoint2D>().enabled = false;
+			box.GetComponent<boxpull>().beingPushed = false;
+			/*Physics2D.queriesStartInColliders = false;
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+			*/
+		}
+
+	}
+
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		/*Gizmos.color = color.yellow;
+		Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x, distance);
+		*/
+		Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+
+
+
+	}
+
+}
